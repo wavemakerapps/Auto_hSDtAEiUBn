8576eee5dbaa095cd73854393f22b3c291ff84d8
ZMlWxmArSN <automate1@wavemaker.com>
2017-09-29 21:16:28 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,14 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "material",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "Auto_hSDtAEiUBn",
+  "homePage" : "Main",
+  "name" : "Auto_hSDtAEiUBn",
+  "platformType" : "WEB",
+  "securityEnabled" : "false",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
